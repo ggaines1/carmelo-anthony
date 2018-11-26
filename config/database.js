c8f7dca64dbb3a10c4780f3f06b70100a63c4641
Glen Gaines <gcgaines7@gmail.com>
2018-11-25 19:25:36 -0500

    carmelo-anthony resubmit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://test:testing1@ds163226.mlab.com:63226/melo-app' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
